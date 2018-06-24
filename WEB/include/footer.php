c0592bd0366c12f11941bd1e99d3b136662d49b4
kim-tairyu <kim.tairyu@gmail.com>
2018-06-25 02:28:27 +0900

    footer

--- WEB/include/footer.php
+++ WEB/include/footer.php
@@ -2,7 +2,7 @@
 <div class="box8">
   <div class="box8-1">
     <p class="copy">
-      Copyright © 2018 MATURI All Rights Reserved<br>
+      Copyright © 2018 MATSURI PLUS All Rights Reserved<br>
     </p>
   </div>
 </div>
