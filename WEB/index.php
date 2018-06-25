6576af7a5ab52d999492b8f452ff5c19d7d26b0c
hashidume masaaki <otaru0105@gmail.com>
2018-06-25 19:02:21 +0900

    WEB修正ver0.2 ・footer下部にあった黄さんのjsの内政治化

--- WEB/index.php
+++ WEB/index.php
@@ -207,7 +207,7 @@ maincontents
 <?php include $pathList->footerPath ?>
 </body>
 <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
-<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+<script src="<?php echo $pathList->jsPath; ?>jquery.min.js"></script>
 <!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
-<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="<?php echo $pathList->jsPath; ?>bootstrap.min.js"></script>
 </html>
